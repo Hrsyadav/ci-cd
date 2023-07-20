aaaf1b607faed9839391f53184c4624c5721b7a3
Hrsyadav <30356923+Hrsyadav@users.noreply.github.com>
2023-07-20 16:49:56 +0530

    added more timeout

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,4 @@ it('Match screenshot with a CI/CD pipeline',async () => {
   render(<App />);
   const screenshot = await generateImage();
   expect(screenshot).toMatchImageSnapshot();
-},10000);
+},50000);
